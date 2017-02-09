64596698199c77afc58629e5dc51bb4efbf749df
IsatisSnowfox <isatis.snowfox@gmail.com>
2017-02-09 15:42:24 +0100

    Setup gulp env

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+/*
+
+Isatis/katbiznis
+
+/gulpfile.js - Gul tasks
+
+coded by Isatis
+
+started at 09/02/2017
+
+*/
+
+var gulp = require("gulp");
+
+// --- Task for images
+
+
+// --- Task for styles
+
+
+// --- Task for pug
+
+
+// --- Task for js
+
+
+// --- Watch Tasks
+
+
+// --- Aliases
